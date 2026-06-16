9ca092da58d65bdeb7612fc356f606285c9c3d7b
alunocbra <alunocbra@ifb.local>
2026-06-16 17:45:57 -0300

    exercicio 3 add

---
+++ exercicio3.js
@@ -0,0 +1,19 @@
+const codigoProduto = 3
+switch (codigoProduto){
+    case 1:
+        console.log ("Hambúrguer");
+        break;
+    case 2:
+         console.log ("Batata frita")
+         break;
+    case 3:
+        console.log ("Refrigerante")
+        break;
+    case 4:
+         console.log ("Milk-shake")
+         break;
+    default:
+        console.log ("Produto não encontrado.")      
+
+
+}
