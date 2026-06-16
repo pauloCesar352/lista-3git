38afdc5d62078e7b61bbf5d871b1dfd7acc9814c
alunocbra <alunocbra@ifb.local>
2026-06-16 17:25:16 -0300

    primeiro exercicio

---
+++ exercicio1.js
@@ -0,0 +1,10 @@
+const numero = 53
+const numero2 = 12
+const soma = numero + numero2
+const subtracao = numero - numero2
+const mult = numero * numero2
+const divisao = (numero / numero2).toFixed(2)
+console.log (`A soma entre o número ${numero} e o número ${numero2} é igual a ${soma}.`)
+console.log (`A multiplicação entre ${numero} e ${numero2} é ${mult}`)
+console.log(`A subtração entre ${numero} e ${numero2} é ${subtracao}`)
+console.log (`A divisão entre ${numero} e ${numero2} é igual a ${divisao}`)
